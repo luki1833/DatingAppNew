a58a2bfe8133297ddcdafcd1d02b7c08af414b0a
FIRST_NAME LAST_NAME <luki1833@gmail.com>
2020-11-30 19:22:05 +0100

    End of section 13+

--- DatingAPP/DatingApp.API/Helpers/Extensions.cs
+++ DatingAPP/DatingApp.API/Helpers/Extensions.cs
@@ -11,7 +11,7 @@ namespace DatingApp.API.Helpers
             response.Headers.Add("Access-Control-Expose-Headers", "Application-Error");
             response.Headers.Add("Access-Control-Allow-Origin", "*");
         }
-
+//sdsds
         public static int CalculateAge(this DateTime theDateTime)
         {
             var age = DateTime.Today.Year - theDateTime.Year;
